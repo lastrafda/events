cabb6ec455c652aaad8d700528b7a0f4f51e011b
José Lastra <lastrafda@gmail.com>
2020-06-21 23:15:59 -0400

    Created a Data-bound component and used @Input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './components/events-list/events-list.component';
+import { EventThumbnailComponent } from './components/event-thumbnail/event-thumbnail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
