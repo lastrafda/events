f1a10079fe13a93002138da981b2dbd0497a995e
José Lastra <lastrafda@gmail.com>
2020-06-22 15:43:37 -0400

    Display a single event
    Accessing route paramaters using ActivatedRoute

--- src/app/components/event-details/event-details.component.ts
+++ src/app/components/event-details/event-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {EventsService} from '../../events.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-event-details',
@@ -10,10 +11,12 @@ export class EventDetailsComponent implements OnInit {
 
   event: any;
 
-  constructor(private eventsService: EventsService) { }
+  constructor(private eventsService: EventsService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.eventsService.getEvent(1);
+    this.event =
+      this.eventsService.getEvent(+this.route.snapshot.params.id);
   }
 
 }
