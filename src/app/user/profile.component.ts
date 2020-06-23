74eb764f26385436a8f5b4532d91a1066af9bdc5
José Lastra <lastrafda@gmail.com>
2020-06-23 13:49:47 -0400

    Added form validations [reactive forms]

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,34 +1,45 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {AuthService} from './auth.service';
 import {Router} from '@angular/router';
 
 @Component({
   templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent {
   profileForm: FormGroup;
+  private firstName: FormControl;
+  private lastName: FormControl;
   constructor(public auth: AuthService,
               private router: Router) {
-
-  }
-
-  ngOnInit(): void {
-    const firstName = new FormControl(this.auth.currentUser.firstName);
-    const lastName = new FormControl(this.auth.currentUser.lastName);
+    this.firstName = new FormControl(
+      this.auth.currentUser.firstName,
+      Validators.required
+    );
+    this.lastName = new FormControl(
+      this.auth.currentUser.lastName,
+      Validators.required
+    );
     this.profileForm = new FormGroup({
-      firstName,
-      lastName,
+      firstName: this.firstName,
+      lastName: this.lastName,
     });
   }
-
 
   cancel() {
     this.router.navigate(['events']);
   }
 
   saveProfile(formValues) {
-    this.auth.updateCurrentUser(formValues.firstName, formValues.lastName);
-    this.router.navigate(['events']);
+    if (this.profileForm.valid){
+      this.auth.updateCurrentUser(formValues.firstName, formValues.lastName);
+      this.router.navigate(['events']);
+    }
+  }
+
+  isValid(fieldName: string) {
+    return this[fieldName].valid
+      || this[fieldName].untouched;
   }
 }
