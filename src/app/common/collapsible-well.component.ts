bfd260cdc36b6ae68fc1f4840adef28cb758823b
José Lastra <lastrafda@gmail.com>
2020-06-25 21:38:21 -0400

    Added an icon for sessions with a lot of voters

--- src/app/common/collapsible-well.component.ts
+++ src/app/common/collapsible-well.component.ts
@@ -4,7 +4,7 @@ import {Component, Input} from "@angular/core";
   selector:  'collapsible-well',
   template: `
   <div (click)="toggleContent()" class="well pointable">
-    <h4 class="well-title">{{title}}</h4>
+    <h4 class="well-title">{{title}} <i *ngIf="voters > 3" class="glyphicon glyphicon-fire" style="color: red; float:right"></i></h4>
     <div #contentWrapper [hidden]="!visible">
       <ng-content>
       </ng-content>
@@ -14,7 +14,8 @@ import {Component, Input} from "@angular/core";
 })
 export class CollapsibleWellComponent {
   @Input() title: string;
-  visible: boolean = true;
+  @Input() voters: number;
+  visible = true;
 
   toggleContent() {
     this.visible = !this.visible;
