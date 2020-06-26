028d3d60a33ab2b16e6ec95999c1e0887820a9ec
José Lastra <lastrafda@gmail.com>
2020-06-25 20:05:56 -0400

    Added filtering sessions functionality

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -1,11 +1,30 @@
-import {Component, Input} from "@angular/core";
-import {ISession} from "../event.model";
+import {Component, Input, OnChanges} from '@angular/core';
+import {ISession} from '../event.model';
 
 @Component({
   selector: 'session-list',
   templateUrl: './session-list.component.html'
 })
-export class SessionListComponent{
+export class SessionListComponent implements OnChanges{
   @Input() sessions: ISession[];
+  @Input() filterBy: string;
+  visibleSessions: ISession[] = [];
 
+  ngOnChanges() {
+    if (this.sessions){
+      // The reason why we are passing this.filterBy as a parameter
+      // is because it makes the method itself stateless.
+      this.filterSessions(this.filterBy);
+    }
+  }
+
+  private filterSessions(filterBy: string) {
+    if (filterBy === 'all'){
+      this.visibleSessions = [...this.sessions];
+    } else {
+      this.visibleSessions = this.sessions.filter(session => {
+        return session.level.toLowerCase() === filterBy;
+      });
+    }
+  }
 }
