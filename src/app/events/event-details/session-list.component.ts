0e45f3ca686a0970687a88b897fa7c1c5d996d55
José Lastra <lastrafda@gmail.com>
2020-06-26 16:43:55 -0400

    Fix: voting on guest

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -26,7 +26,7 @@ export class SessionListComponent implements OnChanges{
   @Input() sortBy: string;
   visibleSessions: ISession[] = [];
 
-  constructor(private auth: AuthService,
+  constructor(public auth: AuthService,
               private voterService: VoterService) {}
 
   ngOnChanges() {
