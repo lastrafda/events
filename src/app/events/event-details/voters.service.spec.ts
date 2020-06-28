5aae155909896f444469ec569b57e8564edf3d28
José Lastra <lastrafda@gmail.com>
2020-06-28 15:09:05 -0400

    Generated a test spec for my voters service
    Learned how to do unit tests with karma and jasmine

---
+++ src/app/events/event-details/voters.service.spec.ts
@@ -0,0 +1,24 @@
+import {VoterService} from './voter.service';
+import {ISession} from '../event.model';
+import {Observable, of} from 'rxjs';
+
+describe('VoterService', () => {
+  let voterService: VoterService;
+  let mockHttp;
+  beforeEach(() => {
+    mockHttp = jasmine.createSpyObj('mockHttp', ['delete', 'post'])
+    voterService = new VoterService(mockHttp);
+  });
+
+  describe('deleteVoter', () => {
+    it('should remove the voter from the list of voters', () => {
+      const session = { id: 6 , voters: ['joe', 'john']};
+      mockHttp.delete.and.returnValue(of(false));
+
+      voterService.deleteVoter(3, session as ISession, 'joe');
+
+      expect(session.voters.length).toBe(1);
+      expect(session.voters[0]).toBe('john');
+    });
+  });
+});
