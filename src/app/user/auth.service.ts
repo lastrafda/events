fc9f13a0d0428f94993a81dfb6c4cf4fb98bb0da
José Lastra <lastrafda@gmail.com>
2020-06-28 01:06:51 -0400

    Added update http call on our auth service

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -34,6 +34,12 @@ export class AuthService {
   updateCurrentUser(firstName: string, lastName: string) {
     this.currentUser.firstName = firstName;
     this.currentUser.lastName = lastName;
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    return this.http.put(`/api/users/${this.currentUser.id}`, this.currentUser, options);
   }
 
   checkAuthenticationStatus() {
